9f26046cda6e1a07d870d294e18533249345c4a3
YACOUBA <yacoubaarama12@gmail.com>
2026-02-14 14:07:23 +0000

    DEPLOYEMENT DE L'APPI FLASK CICD FULL

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from routes.user_routes import *
+from routes.user_routes import *  # ✅ Import unique
+
+import logging
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'ma_cle_secrete_super_securisee'
 
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Importer les routes
-from routes.user_routes import *
+# ✅ Configurer les logs
+logging.basicConfig(
+    filename="app.log",        # fichier log
+    level=logging.INFO,        # niveau de log
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
 
 if __name__ == "__main__":
+    app.logger.info("🚀 Démarrage de l'application Flask sur 0.0.0.0:5001")
     app.run(host="0.0.0.0", port=5001, debug=True)
-
