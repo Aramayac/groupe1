3fd2c0824ee16718e9aefcd79acbbe3db87af849
YACOUBA <yacoubaarama12@gmail.com>
2026-02-14 13:21:14 +0000

    DEPLOYEMENT DE L'APPI FLASK CICD V2

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ migrate = Migrate(app, db)
 from routes.user_routes import *
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
 
